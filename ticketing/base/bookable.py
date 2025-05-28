32da57296379089c40a7d97724f3f4d876a27857
sabareesh yarru <sabareeshy@gmail.com>
2025-05-28 21:36:53 +0530

    added ticketing

--- ticketing/base/bookable.py
+++ ticketing/base/bookable.py
@@ -1,28 +1,23 @@
-from .seat import Seat
+from seat import Seat
 
 class Bookable:
-    """This is parent class where booking seats is involved
-    """
-    """This represents bus during bus booking
-    """
     def __init__(self, operator, name, registration_number):
-
         self.operator = operator
         self.name = name
         self.registration_number = registration_number
         self.seats = dict()
 
-    def set_seats(self, count, rows, columns):
+    def set_seats(self, rows, columns):
 
-        """This creates default seat structure
-        """
         for row_index in range(1, rows+1):
-           for column_index in range(1, columns+1):
-               seat = Seat(
-                   id=f"{row_index}_{column_index}"
-               )
-               self.seats.update({seat.get_seat_number,seat})
-            
+            for col_index in range(1, columns+1):
+                seat = Seat(id=f"{row_index}_{col_index}")
+            self.seats[seat.get_seat_number()] = seat
+    
     def book_ticket(self, seat_number, price):
 
-        self.seats.get(seat_number).book(price)
+        self.seat.get(seat_number).book(price)
+
+
+        
+
